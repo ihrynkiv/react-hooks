0c551bcea90ab27863fe68091412063db2d8f10c
Ivan Hrynkiv <ihrynkiv@intelliarts.com>
2021-08-01 17:36:43 +0300

    configure starter file for useState

--- src/hooks-examples/state.js
+++ src/hooks-examples/state.js
@@ -1,53 +1,21 @@
 import React, {useState} from 'react'
 
-// function computeInitialCounter() {
-//   console.log('Some calculations...')
-//   return Math.trunc(Math.random() * 20)
-// }
-
-function App() {
-  const [counter, setCounter] = useState(0)
-  // const [counter, setCounter] = useState(computeInitialCounter())
-  // const [counter, setCounter] = useState(() => {
-  //   return computeInitialCounter()
-  // })
-
-  const [state, setState] = useState({
-    title: 'Лічильник',
-    date: Date.now()
-  })
+export default function App() {
+  let myCounter = 0;
 
   function increment() {
-    setCounter(counter + 1)
-    // setCounter((prevCounter) => {
-    //   return prevCounter + 1
-    // })
-    // setCounter(prev => prev + 1)
+
   }
 
   function decrement() {
-    setCounter(counter - 1)
-  }
 
-  function updateTitle() {
-    setState(prev => {
-      return {
-        ...prev,
-        title: 'Орест топ чєл!'
-      }
-    })
   }
 
   return (
     <div>
-      <h1>Счетчик: {counter}</h1>
+      <h1>Лічильник: {myCounter}</h1>
       <button onClick={increment} className="btn btn-outline-success">Додати</button>
       <button onClick={decrement} className="btn btn-outline-danger">Видалити</button>
-      <button onClick={updateTitle} className="btn btn-outline-dark">Змінити назву</button>
-
-      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   )
 }
-
-export default App
