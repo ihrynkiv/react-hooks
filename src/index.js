f7c30521d27173e93d914b59328a8cbd605a519a
Ivan Hrynkiv <ihrynkiv@intelliarts.com>
2021-08-01 15:39:17 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './hooks-examples/state';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
