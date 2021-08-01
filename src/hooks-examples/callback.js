63354f4de689d4c3833d61960780a8c6424de918
Ivan Hrynkiv <ihrynkiv@intelliarts.com>
2021-08-01 19:48:42 +0300

    configure file for useCallback

--- src/hooks-examples/callback.js
+++ src/hooks-examples/callback.js
@@ -9,27 +9,27 @@ function App() {
     color: colored ? 'darkred' : 'black'
   }
 
-  // const generateItemsFromAPI = (startIndex) => {
-  // console.log("generateItemsFromAPI");
-  // const result = [];
-  // for (let i = 0; i < count; i++) {
-  //   result.push(`Елемент ${i + startIndex}`)
-  // }
-  // return result;
-  // // }
+  const generateItemsFromAPI = (startIndex) => {
+  console.log("generateItemsFromAPI");
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    result.push(`Елемент ${i + startIndex}`)
+  }
+  return result;
+  }
 
-  const generateItemsFromAPI = useCallback((startIndex) => {
-    console.log("generateItemsFromAPI");
-    const result = [];
-    for (let i = 0; i < count; i++) {
-      result.push(`Елемент ${i + startIndex}`)
-    }
-    return result;
-  }, [count])
+  // const generateItemsFromAPI = useCallback((startIndex) => {
+  //   console.log("generateItemsFromAPI");
+  //   const result = [];
+  //   for (let i = 0; i < count; i++) {
+  //     result.push(`Елемент ${i + startIndex}`)
+  //   }
+  //   return result;
+  // }, [count])
 
   return (
     <>
-      <h1 style={styles}>Количество элементов: {count}</h1>
+      <h1 style={styles}>Кількість елементів: {count}</h1>
       <button className={'btn btn-success'} onClick={() => setCount(prev => prev + 1)}>Додати</button>
       <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}>Змінити</button>
 
